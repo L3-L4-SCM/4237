4c77410d736e6255645d83f4122adb226fc8b1fe
Navya G <navya.g@s.amity.edu>
2025-05-26 09:38:17 +0530

    Update simple.c

--- simple.c
+++ simple.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  int a,b;
+  int a,b; sum=0;
   printf("enter the value");
   scanf("%d %d",&a,&b);
-
-}
+   sum = a+b;
+  printf("the sum is:");
+  scanf("%d",&sum);
+  }
